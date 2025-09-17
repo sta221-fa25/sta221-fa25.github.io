9a23e9e38fc05e0bbc80205d9ab77fd31d8d6897
athos00 <26285561+athos00@users.noreply.github.com>
2025-09-17 13:49:23 -0400

    updates

---
+++ admin-scripts/teamRepos.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(ghclass)
+
+
+## Make teams (once)
+
+all_team <- readxl::read_xlsx("~/Downloads/teams_221.xlsx")
+
+team_names <- all_team %>%
+  select(team) %>%
+  pull()
+
+org <- "sta221-fa25"
+
+# create teams
+team_create(org, team_names)
+
+# invite to teams
+for(team_name in team_names) {
+  users = all_team %>%
+    filter(team == team_name) %>%
+    select(github) %>%
+    pull()
+  
+  team_invite(org, user = users, team = team_name)
+}
+
+
+###########################
+#### deploy team repos ####
+###########################
+
+# example data frame for demo purposes
+# you will need to format your data frame to look like this
+roster = all_team
+
+# edit each item below
+org_create_assignment(
+  org = "sta221-fa25",
+  user = roster$github,
+  repo = paste0("lab-03-", roster$team), ## edit this
+  team = roster$team,
+  source_repo = "sta221-fa25/lab-03", ## edit this
+  private = TRUE
+)
